9a15f2a88d281a85a33a06249ba3e86b21e3c35f
Richard <proxyserver7798@gmail.com>
2025-09-17 11:53:01 +0200

    Start of day commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import Banner from './components/Banner/Banner';
-import Companies from './components/Companies/Companies';
-import Buyers from './components/Buyers/index';
-import Provide from './components/Provide/index';
-import Why from './components/Why/index';
-import Network from './components/Network/index';
-import Clientsay from './components/Clientsay/index';
-import Newsletter from './components/Newsletter/Newsletter';
-
+import Banner from "./components/Banner/Banner";
+import Companies from "./components/Companies/Companies";
+import Buyers from "./components/Buyers/index";
+import Provide from "./components/Provide/index";
+import Why from "./components/Why/index";
+import Network from "./components/Network/index";
+import Clientsay from "./components/Clientsay/index";
+import Newsletter from "./components/Newsletter/Newsletter";
 
 export default function Home() {
   return (
@@ -16,9 +15,9 @@ export default function Home() {
       <Buyers />
       <Provide />
       <Why />
-      <Network />
+      {/* <Network /> */}
       <Clientsay />
       <Newsletter />
     </main>
-  )
+  );
 }
